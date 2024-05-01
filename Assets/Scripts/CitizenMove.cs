aa2ae3cef5e6fdeb9fb93fdc2848594f137e12fb
ozing1111 <68819272+ozing1111@users.noreply.github.com>
2024-05-01 09:17:48 +0900

    1. 시민과 적 AI 구현

---
+++ Assets/Scripts/CitizenMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitizenMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    public int nextMove;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Invoke("Think", 5);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.2f, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Tilemap"));
+
+        if (rayHit.collider == null)
+        {
+            Turn();
+        }
+    }
+
+    void Think()
+    {
+        nextMove = Random.Range(-1, 2);
+        anim.SetInteger("WalkSpeed", nextMove);
+
+        if (nextMove != 0)
+        {
+            spriteRenderer.flipX = (nextMove == 1);
+        }
+
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void Turn()
+    {
+        nextMove = nextMove * (-1);
+        spriteRenderer.flipX = (nextMove == 1);
+
+        CancelInvoke();
+        Invoke("Think", 2);
+    }
+}
